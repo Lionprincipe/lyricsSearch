d405f01698cce31c4a3deb28db145cf78a5d7bdc
Lionel <lionprincipe@hotmail.com>
2018-10-03 09:09:24 +0200

    add Lyrics axios request start styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Lyrics from './components/tracks/Lyrics'
+import { Provider } from './context'
 
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+                <Route path="/lyrics/track/:id" component={Lyrics} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     )
   }
 }
